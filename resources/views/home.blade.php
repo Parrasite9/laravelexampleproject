31edf3d26da280d3c42183ebb202ac85c575ec51
isaiah <isaiah.johnson@bigdco.com>
2025-11-24 15:23:20 -0600

    Created navlink component and imported tailwind Navbar component for styling.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <h1>This is the home page</h1>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h1 className="text-red-700">This is the home page</h1>
 </x-layout>
