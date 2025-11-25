4cbf2534221a4f16e4ce52e687aab1df9f0db60e
isaiah <isaiah.johnson@bigdco.com>
2025-11-25 08:40:58 -0600

    Active links in the navbar are now set up

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,2 +1,13 @@
+@props(['active' => false, 'href'])
 
-    <a href="/">{{ $slot }}</a>
+{{-- <a href="/" class="{{ $active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+aria-current="{{ $active ? 'page' : 'false'}}"
+    >
+    {{ $slot }}
+</a> --}}
+
+<a href="{{ $href }}" class="{{$active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false'}}"
+    >
+    {{ $slot }}
+</a>
